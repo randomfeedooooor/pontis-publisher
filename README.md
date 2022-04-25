8c25fd3a3e8650ee98cda5de3206950c7dd45d9c
Rootul Patel <rootulp@gmail.com>
2022-04-25 15:49:33 +0200

    Done with V1

--- README.md
+++ README.md
@@ -26,6 +26,11 @@ docker run --env-file .secrets.env -t pontis-publisher
 1. Navigate to <https://goerli.voyager.online/contract/0x039d1bb4904cef28755c59f081cc88a576ecdf42240fb73dd44ddd003848ce33#readContract>
 1. Use `get_value` with `key=491260896305`
 
+## Tasks
+
+- [x] Publish a constant number
+- [x] Publish a random number
+
 ## Links
 
 1. <https://cairo-utils-web.vercel.app/>
--- fetch-and-publish.py
+++ fetch-and-publish.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
-from hashlib import sha256
-from datetime import datetime
+import random
+import datetime
 
 from pontis.core.const import NETWORK, ORACLE_ADDRESS
 from pontis.core.utils import str_to_felt
@@ -9,13 +9,19 @@ from pontis.publisher.client import PontisPublisherClient
 from starkware.crypto.signature.signature import sign
 
 DECIMALS = 18
+FELT_SIZE = 2 ** 252
 
 async def main():
     PUBLISHER_PRIVATE_KEY = int(os.environ.get("PUBLISHER_PRIVATE_KEY"))
     PUBLISHER = "randomfeedooooor"
     KEY = "rand1"
-    RANDOM_NUMBER = 422
-    timestamp = 3
+
+    random_number = random.randrange(FELT_SIZE)
+    timestamp = int(
+                datetime.datetime.now(datetime.timezone.utc)
+                .replace(tzinfo=datetime.timezone.utc)
+                .timestamp()
+            )
 
     # Step 1. Register publisher by providing the signed publisher to Pontis
     # team
@@ -26,9 +32,9 @@ async def main():
         ORACLE_ADDRESS, PUBLISHER_PRIVATE_KEY, PUBLISHER, network=NETWORK
     )
 
-    await client.publish(KEY, RANDOM_NUMBER, timestamp)
+    await client.publish(KEY, random_number, timestamp)
 
-    print(f"Submitted random number {RANDOM_NUMBER} for {PUBLISHER} under key {KEY} at timestamp {timestamp}")
+    print(f"Submitted random number {random_number} at timestamp {timestamp} for {PUBLISHER} under key {KEY}")
 
 if __name__ == "__main__":
     asyncio.run(main())
