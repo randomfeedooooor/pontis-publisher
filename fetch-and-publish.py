59e2fc6f11039b98ba2323b178609e35367df43f
Rootul Patel <rootulp@gmail.com>
2022-04-25 19:26:24 +0200

    Reorder code

--- fetch-and-publish.py
+++ fetch-and-publish.py
@@ -11,12 +11,8 @@ from pontis.publisher.client import PontisPublisherClient
 from starkware.crypto.signature.signature import sign
 from starkware.cairo.lang.vm.crypto import pedersen_hash
 
-DECIMALS = 18
+SLEEP_INTERVAL = 30 # seconds
 P = 2 ** 251 + 17 * 2 ** 192 + 1
-SLEEP_INTERVAL = 30
-
-# def hex_to_felt(val):
-#     return int(val, 16)
 
 def hex_to_felt(val):
     return hex_to_int(val) % P
@@ -28,10 +24,22 @@ def splitRandomness(val):
     return val[:32], val[32:]
 
 async def main():
+
+    # Constants
     PUBLISHER_PRIVATE_KEY = int(os.environ.get("PUBLISHER_PRIVATE_KEY"))
     PUBLISHER = "randomfeedooooor"
     KEY = "rand1"
 
+    # Register publisher by providing the signed publisher to Pontis team
+    # signed = sign(str_to_felt(PUBLISHER), PUBLISHER_PRIVATE_KEY)
+    # print(f"Signed publisher {signed}")
+
+    # Initialize client
+    client = PontisPublisherClient(
+        ORACLE_ADDRESS, PUBLISHER_PRIVATE_KEY, PUBLISHER, network=NETWORK
+    )
+
+    # Get random number from drand and conver to felt
     url = "https://drand.cloudflare.com"
     request_path = "/public/latest"
     method = "GET"
@@ -42,28 +50,17 @@ async def main():
     randomness = result["randomness"]
     part1, part2 = splitRandomness(randomness)
     randomnessFelt = pedersen_hash(hex_to_int(part1), hex_to_int(part2))
-    # randomnessFelt = hex_to_felt(randomness)
     print(f"[INFO] drand result {result}")
-    print(f"[INFO] randomness {randomness}, randomnessFelt {randomnessFelt}")
+    print(f"[INFO] randomness {randomness}")
+    print(f"[INFO] randomnessFelt {randomnessFelt}")
 
-    # random_number = random.randrange(FELT_SIZE)
     timestamp = int(
                 datetime.datetime.now(datetime.timezone.utc)
                 .replace(tzinfo=datetime.timezone.utc)
                 .timestamp()
             )
 
-    # Step 1. Register publisher by providing the signed publisher to Pontis
-    # team
-    # signed = sign(str_to_felt(PUBLISHER), PUBLISHER_PRIVATE_KEY)
-    # print(f"Signed {signed}")
-
-    client = PontisPublisherClient(
-        ORACLE_ADDRESS, PUBLISHER_PRIVATE_KEY, PUBLISHER, network=NETWORK
-    )
-
     await client.publish(KEY, randomnessFelt, timestamp)
-
     print(f"Submitted random number {randomnessFelt} at timestamp {timestamp} for {PUBLISHER} under key {KEY}")
 
 
